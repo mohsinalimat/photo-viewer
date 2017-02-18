8eee650430c11a29c50b8ff143c22958684752d6
Mohamed Hamed <hamed.fci@gmail.com>
2017-02-18 23:08:39 +0200

    Added single tap to hide views

--- PhotoViewer/PhotoViewer/PhotoViewerViewController.swift
+++ PhotoViewer/PhotoViewer/PhotoViewerViewController.swift
@@ -12,22 +12,59 @@ class PhotoViewerViewController: InteractiveViewController , UIScrollViewDelegat
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var actionView: UIView!
+   
     
+    var hideViews: Bool = true
     var image : UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        initScrollView()
+        initNavbar()
         
+    }
+    
+    func doneTapped(sender: Any) {
+        dismiss(animated: true) { 
+          print("dismissd")
+        }
+    }
+    
+    func initNavbar() {
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 44))
+        self.view.addSubview(navBar);
+        let navItem = UINavigationItem(title: "");
+        navBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white]
+        navBar.tintColor = .white
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action:  #selector(doneTapped(sender:)))
+        navItem.leftBarButtonItem = doneButton
+        
+        navBar.setItems([navItem], animated: false);
+        transparentNavbar(bar: navBar)
+    }
+    
+    func transparentNavbar(bar: UINavigationBar) {
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        bar.shadowImage = UIImage()
+    }
+    
+    //Mark ScrollView
+    func initScrollView() {
         scrollView.delegate = self
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 6.0
-        
-        // Listen for Double Tap Zoom
+        //Double Tap Zoom
         let doubleTap = UITapGestureRecognizer(target: self, action:  #selector(handleDoubleTapScrollView(recognizer:)))
         doubleTap.numberOfTapsRequired = 2
         scrollView.addGestureRecognizer(doubleTap)
+        //Single Tap 
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleSingleScrollView(recognizer:)))
+        singleTap.numberOfTapsRequired = 1
+        scrollView.addGestureRecognizer(singleTap)
         
+        singleTap.require(toFail: doubleTap)
     }
 
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
@@ -39,6 +76,18 @@ class PhotoViewerViewController: InteractiveViewController , UIScrollViewDelegat
             scrollView.zoom(to: zoomRectForScale(scale: scrollView.maximumZoomScale, center: recognizer.location(in: recognizer.view)), animated: true)
         } else {
             scrollView.setZoomScale(1, animated: true)
+        }
+    }
+    
+    func handleDoubleSingleScrollView(recognizer: UITapGestureRecognizer) {
+        hideViews(hide: hideViews)
+        hideViews = !hideViews
+
+    }
+    
+    func hideViews(hide: Bool) {
+        UIView.animate(withDuration: 5) {
+            self.actionView.isHidden = hide
         }
     }
     
